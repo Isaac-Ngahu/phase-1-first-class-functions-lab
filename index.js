e3058c1433efbdd5ee2d4e525b06aac8ea51a181
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-03-29 16:22:48 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers=function(){
+    
+    const newArray=drivers.slice(0,2);
+    return newArray;
+}
+const returnLastTwoDrivers=function(){
+const newArray=drivers.slice(-2);
+return newArray;
+}
+
+ const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+ function createFareMultiplier(integer){
+    return ((integer)=>integer*5);
+ }
+ const fareMultiplier=createFareMultiplier();
+ const fareDoubler=function(fare){
+return fare*2;
+ }
+ const fareTripler=function(fare){
+    return fare*3;
+ }
+function selectDifferentDrivers(arrayofdrivers,fname){
+    return fname();
+}
